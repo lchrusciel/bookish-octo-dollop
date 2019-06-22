3ff5d32cc7346c887cd920d2e3a2ce25b82cc1bd
Łukasz Chruściel <lchrusciel@gmail.com>
2019-06-22 23:25:27 +0200

    Add dynamic js content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log('Elo 🚀');
+
+const dynamicElement = document.querySelector('.about__content--js');
+
+if (dynamicElement !== null) {
+    dynamicElement.innerHTML = 'Dynamicly injected with JS';
+}
