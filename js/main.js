c106a562b52fe0b36d6789c18408df41eb6fa423
Łukasz Chruściel <lchrusciel@gmail.com>
2019-06-23 11:20:05 +0200

    JS tests

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ const dynamicElement = document.querySelector('.about__content--js');
 if (dynamicElement !== null) {
     dynamicElement.innerHTML = 'Dynamicly injected with JS';
 }
+
+console.log(calculate(1));
+
+function calculate(x) {
+    x = x + 3;
+    console.log(`Normal: ${x}`);
+
+    return x * 7;
+}
+
+console.log(calculate(2));
+
+const calculateFat = x => (x+3)*7;
+
+console.log(calculateFat(1));
+console.log(calculateFat(2));
