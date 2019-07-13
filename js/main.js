99b56f997c995e5a9709acb93c78e5e6e6c7fa0f
Łukasz Chruściel <lchrusciel@gmail.com>
2019-07-13 17:56:07 +0200

    Add hamburger menu

--- js/main.js
+++ js/main.js
@@ -10,3 +10,10 @@ const age = 40;
 hello('John', 23);
 hello(user1, age);
 hello(user2, 23);
+
+let toggle = document.querySelector('.navigation-menu-toggle__button--js');
+let navigation = document.querySelector('.navigation--js');
+
+toggle.addEventListener('click', () => {
+    navigation.classList.toggle('navigation--visible-js');
+});
